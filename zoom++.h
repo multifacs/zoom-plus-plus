6067bce0e0e30373cf6952a25f964d28312ff9aa
multifacs <multifacs@users.noreply.github.com>
2022-08-14 18:45:16 +0300

    initial commit

---
+++ zoom++.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#define rn ;
+#define chief main
+#define cap false  // use instead of or null or EOF when appropriate
+#define fr long
+#define no !
+#define sussin ?
+#define bussin :
+#define bouta &
+#define finna =
+#define mf *
+#define be ==
+#define vibin --
+#define ongod ++
+#define yikes break
+#define deadass return
+#define yo {
+#define tho }
